c4c74f6d74dc81b97bf3c61441ee35cd60918a22
fbugarski <filipbugarski@gmail.com>
2022-03-25 15:49:06 +0100

    Fix responses
    Signed-off-by: fbugarski <filipbugarski@gmail.com>

--- pkg/sdk/go/responses.go
+++ pkg/sdk/go/responses.go
@@ -1,19 +1,1 @@
 package sdk
-
-type computationRes struct {
-	ID       string                 `json:"ID,omitempty"`
-	Name     string                 `json:"name,omitempty"`
-	Key      string                 `json:"key,omitempty"`
-	Metadata map[string]interface{} `json:"metadata,omitempty"`
-}
-
-type ComputationsPage struct {
-	Computation []Computation `json:"computations"`
-	pageRes
-}
-
-type pageRes struct {
-	Total  uint64 `json:"total"`
-	Offset uint64 `json:"offset"`
-	Limit  uint64 `json:"limit"`
-}
