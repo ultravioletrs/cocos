0049ce2b289de0bdde29176ed45a8ed4f4204a15
Darko Draskovic <darko.draskovic@gmail.com>
2023-08-18 11:47:11 +0200

    Add temporary sh script to start vm used for testing
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

---
+++ cmd/manager/startVM.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Mapping CTRL-C to CTRL-]"
+stty intr ^]
+
+echo "Launching VM ..."
+/usr/bin/qemu-system-x86_64 -enable-kvm -machine q35 -cpu EPYC -smp 4,maxcpus=64 -m 2048M,slots=5,maxmem=30G -drive if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=img/OVMF_VARS.fd -device virtio-scsi-pci,id=scsi,disable-legacy=on,iommu_platform=true -drive file=img/focal-server-cloudimg-amd64.img,if=none,id=disk0,format=qcow2 -device scsi-hd,drive=disk0 -netdev user,id=vmnic,hostfwd=tcp::2222-:22,hostfwd=tcp::9301-:9031,hostfwd=tcp::7020-:7002 -device virtio-net-pci,disable-legacy=on,iommu_platform=true,netdev=vmnic,romfile= -nographic -monitor pty
+
+# restore the mapping
+stty intr ^c
