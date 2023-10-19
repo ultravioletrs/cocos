a198da6008397d8e338db7de0e3e28ca4ae356b7
WashingtonKK <washingtonkigan@gmail.com>
2023-10-19 19:43:24 +0300

    Return agent changes
    Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

--- agent/service.go
+++ agent/service.go
@@ -111,18 +111,9 @@ func (as *agentService) Result(ctx context.Context) ([]byte, error) {
 func (as *agentService) Attestation(ctx context.Context) ([]byte, error) {
 	// Implement the logic for the Attestation method here
 	// Use the provided ctx parameter as needed
+	var attestation []byte
 
-	attestation := `-----BEGIN RSA PRIVATE KEY-----
-	MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu
-	KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm
-	o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k
-	TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAevXysE2RbFDYdAiEBCUEaRQnMnbp7
-	9mxDXDf6AU0cN/RPBjb9qSHDcWZHGzUCIG2Es59z8ugGrDY+pxLQnwfotadxd+Uy
-	v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs
-	/5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00
-	-----END RSA PRIVATE KEY-----`
-
-	as.attestation = []byte(attestation)
+	as.attestation = attestation
 
 	return as.attestation, nil
 }
