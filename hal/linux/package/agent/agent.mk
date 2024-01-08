3647aff5f4416543eff67217796507d37f8e6ad5
danko-miladinovic <danko@etf.rs>
2024-01-08 14:53:15 +0100

    Use systemd for HAL boot

--- hal/linux/package/agent/agent.mk
+++ hal/linux/package/agent/agent.mk
@@ -13,6 +13,11 @@ endef
 
 define AGENT_INSTALL_TARGET_CMDS
 	cp $(@D)/build/cocos-agent $(TARGET_DIR)/bin
+	mkdir -p $(TARGET_DIR)/var/log/cocos
+endef
+
+define AGENT_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(@D)/init/systemd/cocos-agent.service $(TARGET_DIR)/usr/lib/systemd/system/cocos-agent.service
 endef
 
 $(eval $(golang-package))
