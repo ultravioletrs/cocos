79f67c66c03d9aefb7aed293fc77933ac25c68d0
danko-miladinovic <danko@etf.rs>
2024-01-15 17:55:50 +0100

    Add ETH_IFACE variable

--- init/systemd/agent_start_script.sh
+++ init/systemd/agent_start_script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-ip link set dev enp0s2 up
-dhclient enp0s2
-AGENT_GRPC_HOST=$(ip -4 addr show enp0s2 | grep inet | awk '{print $2}' | cut -d/ -f1)
+ETH_IFACE=enp0s2
+
+ip link set dev $ETH_IFACE up
+dhclient $ETH_IFACE
+AGENT_GRPC_HOST=$(ip -4 addr show $ETH_IFACE | grep inet | awk '{print $2}' | cut -d/ -f1)
 
 export AGENT_GRPC_HOST
 
