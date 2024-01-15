589b12aa0fe476dddde095f1ec25862b2c90f6d6
danko-miladinovic <danko@etf.rs>
2024-01-15 18:38:41 +0100

    Further explain ETH_IFACE value

--- init/systemd/agent_start_script.sh
+++ init/systemd/agent_start_script.sh
@@ -3,12 +3,13 @@
 # The variable ETH_IFACE contains the name the systemd gave to the network interface. 
 # The systemd configures the name based on the QEMU parameters. 
 # The parts of the name enp0s2 mean:
-#   et - ethernet card. It means this is the ethernet interface.
-#   p - means that the interface is connected to a PCI bus.
-#   0 - the interface is connected to bus 0.
-#   s2 -the interface is connected to slot 2.
-#
-# The variable should be configured through QEMU parameters for the network device,
+#  et - ethernet card. It means this is the ethernet interface.
+#  p - means that the interface is connected to a PCI bus.
+#  0 - the interface is connected to bus 0.
+#  s2 -the interface is connected to slot 2.
+
+# The variable ETH_IFACE value must match the name configured through QEMU parameters for the network device. 
+# The bus number and slot number are configured through QEMU device parameters, parameters
 # addr (for slot number), and bus (for bus number).
 ETH_IFACE=enp0s2
 
