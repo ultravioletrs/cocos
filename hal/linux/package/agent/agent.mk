0aba8ee6eecd1f0c978bd76bb153f88ccefb7df9
danko-miladinovic <danko@etf.rs>
2024-01-15 17:38:57 +0100

    Use systemd for naming network interfaces

--- hal/linux/package/agent/agent.mk
+++ hal/linux/package/agent/agent.mk
@@ -14,11 +14,12 @@ endef
 define AGENT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/build/cocos-agent $(TARGET_DIR)/bin
 	mkdir -p $(TARGET_DIR)/var/log/cocos
+	mkdir -p $(TARGET_DIR)/cocos/
 endef
 
 define AGENT_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(@D)/init/systemd/cocos-agent.service $(TARGET_DIR)/usr/lib/systemd/system/cocos-agent.service
-	$(INSTALL) -D -m 0755 $(@D)/init/systemd/agent_start_script.sh $(TARGET_DIR)/bin/agent_start_script.sh
+	$(INSTALL) -D -m 0755 $(@D)/init/systemd/agent_start_script.sh $(TARGET_DIR)/cocos/agent_start_script.sh
 	$(INSTALL) -D -m 0644 $(@D)/init/systemd/00-network.link $(TARGET_DIR)/etc/systemd/network
 endef
 
--- init/systemd/agent_start_script.sh
+++ init/systemd/agent_start_script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-dhclient eth0
-AGENT_GRPC_HOST=$(ip -4 addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1)
+ip link set dev enp0s2 up
+dhclient enp0s2
+AGENT_GRPC_HOST=$(ip -4 addr show enp0s2 | grep inet | awk '{print $2}' | cut -d/ -f1)
 
 export AGENT_GRPC_HOST
 
