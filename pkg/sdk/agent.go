6e93c505dcbb54406a7fa8955267d41b74772b41
SammyOina <sammyoina@gmail.com>
2024-01-24 14:09:54 +0300

    Improve attestation and update deps
    Enhanced the attestation process to require additional report data and a variable privacy level, catering to varying security requirements. The `Run` method's context parameter was removed to simplify invocation following API alignment. Dependencies got updated, notably adding `github.com/google/go-sev-guest` for attestation, and some were indirectly bumped. These updates ensure the agent service leverages the latest available features and improvements for security and stability.
    
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- pkg/sdk/agent.go
+++ pkg/sdk/agent.go
@@ -23,7 +23,7 @@ func NewAgentSDK(log *slog.Logger, agentClient agent.AgentServiceClient) *agentS
 	}
 }
 
-func (sdk *agentSDK) Run(ctx context.Context, ac agent.Computation) (string, error) {
+func (sdk *agentSDK) Run(ac agent.Computation) (string, error) {
 	return "", nil
 }
 
@@ -73,7 +73,7 @@ func (sdk *agentSDK) Result(ctx context.Context, consumer string) ([]byte, error
 	return response.File, nil
 }
 
-func (sdk *agentSDK) Attestation(ctx context.Context) ([]byte, error) {
+func (sdk *agentSDK) Attestation(ctx context.Context, reportData []byte, vpml uint) ([]byte, error) {
 	request := &agent.AttestationRequest{}
 
 	response, err := sdk.client.Attestation(ctx, request)
