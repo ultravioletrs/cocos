cdf174dbd3a6e5cfc24bce67db51f159734503c6
SammyOina <sammyoina@gmail.com>
2024-02-27 16:11:43 +0300

    Simplify attestation validation process
    Refactored the attestation CLI command to improve usability by removing the need for explicitly provided report data as an argument. Default values for configuration parameters have been centralized into constants for maintainability. Additionally, integrated data parsing functions to streamline the validation checks with robust error handling for cases such as invalid hex strings and file I/O issues.
    
    This enhancement reduces user error and the complexity of entering attestation data by parsing relevant information from within the environment, all while maintaining the same security standards.
    
    The process of marking flags as required for CLI commands has been corrected to prevent runtime errors and improve command reliability.
    
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- cli/attestation.go
+++ cli/attestation.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/google/go-sev-guest/abi"
@@ -21,7 +23,20 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
-const reportDataLen = 64
+const (
+	reportDataLen                    = 64
+	defaultMinimumTcb                = 0
+	defaultMinimumLaunchTcb          = 0
+	defaultMinimumGuestSvn           = (1 << 17)
+	defaultMinimumBuild              = 0
+	defaultCheckCrl                  = false
+	defaultDisallowNetwork           = false
+	defaultTimeout                   = 2 * time.Minute
+	defaultMaxRetryDelay             = 30 * time.Second
+	defaultRequireAuthor             = false
+	defaultRequireIdBlock            = false
+	defaultPermitProvisionalSoftware = false
+)
 
 var (
 	cfg                 check.Config
@@ -104,9 +119,9 @@ func (cli *CLI) NewGetAttestationCmd() *cobra.Command {
 func (cli *CLI) NewValidateAttestationValidationCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "validate",
-		Short:   "Validate and verify attestation information. The report and report data are provided in encoded hex string.",
-		Example: "validate <attestation_report> <report_data>",
-		Args:    cobra.ExactArgs(2),
+		Short:   "Validate and verify attestation information. The report is provided in encoded hex string.",
+		Example: "validate <attestation_report>",
+		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Println("Checking attestation")
 
@@ -114,17 +129,22 @@ func (cli *CLI) NewValidateAttestationValidationCmd() *cobra.Command {
 			if err != nil {
 				log.Fatalf("attestation validation and veification failed with error: %s", err)
 			}
-			report_data, err := hex.DecodeString(args[1])
-			if err != nil {
-				log.Fatalf("attestation validation and veification failed with error: %s", err)
-			}
 
 			if err := parseConfig(); err != nil {
 				log.Fatalf("attestation validation and veification failed with error: %s", err)
 			}
+			if err := parseHashes(); err != nil {
+				log.Fatalf("attestation validation and veification failed with error: %s", err)
+			}
+			if err := parseFiles(); err != nil {
+				log.Fatalf("attestation validation and veification failed with error: %s", err)
+			}
+			if err := parseUints(); err != nil {
+				log.Fatalf("attestation validation and veification failed with error: %s", err)
+			}
 			cfg.Policy.Vmpl = wrapperspb.UInt32(0)
 
-			if err := verifyAndValidateAttestation(report, report_data); err != nil {
+			if err := verifyAndValidateAttestation(report); err != nil {
 				log.Fatalf("attestation validation and veification failed with error: %s", err)
 			}
 			log.Println("Attestation validation and verification is successful!")
@@ -133,23 +153,24 @@ func (cli *CLI) NewValidateAttestationValidationCmd() *cobra.Command {
 	cmd.Flags().StringVar(&cfgString, "config", "", "Serialized json check.Config protobuf. This will overwrite individual flags. Unmarshalled as json. Example: "+`
 	{"rootOfTrust":{"product":"test_product","cabundlePaths":["test_cabundlePaths"],"cabundles":["test_Cabundles"],"checkCrl":true,"disallowNetwork":true},"policy":{"minimumGuestSvn":1,"policy":"1","familyId":"AQIDBAUGBwgJCgsMDQ4PEA==","imageId":"AQIDBAUGBwgJCgsMDQ4PEA==","vmpl":0,"minimumTcb":"1","minimumLaunchTcb":"1","platformInfo":"1","requireAuthorKey":true,"reportData":"J+60aXs8btm8VcGgaJYURGeNCu0FIyWMFXQ7ZUlJDC0FJGJizJsOzDIXgQ75UtPC+Zqe0A3dvnnf5VEeQ61RTg==","measurement":"8s78ewoX7Xkfy1qsgVnkZwLDotD768Nqt6qTL5wtQOxHsLczipKM6bhDmWiHLdP4","hostData":"GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw=","reportId":"GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw=","reportIdMa":"GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw=","chipId":"J+60aXs8btm8VcGgaJYURGeNCu0FIyWMFXQ7ZUlJDC0FJGJizJsOzDIXgQ75UtPC+Zqe0A3dvnnf5VEeQ61RTg==","minimumBuild":1,"minimumVersion":"0.90","permitProvisionalFirmware":true,"requireIdBlock":true,"trustedAuthorKeys":["GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw="],"trustedAuthorKeyHashes":["GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw="],"trustedIdKeys":["GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw="],"trustedIdKeyHashes":["GSvLKpfu59Y9QOF6vhq0vQsOIvb4+5O/UOHLGLBTkdw="],"product":{"name":"SEV_PRODUCT_MILAN","stepping":1,"machineStepping":1}}}`)
 	cmd.Flags().BytesHexVar(&cfg.Policy.HostData, "host_data", []byte{}, "The expected HOST_DATA field as a hex string. Must encode 32 bytes. Unchecked if unset.")
+	cmd.Flags().BytesHexVar(&cfg.Policy.ReportData, "report_data", []byte{}, "The expected REPORT_DATA field as a hex string. Must encode 64 bytes. Must be set.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.FamilyId, "family_id", []byte{}, "The expected FAMILY_ID field as a hex string. Must encode 16 bytes. Unchecked if unset.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.ImageId, "image_id", []byte{}, "The expected IMAGE_ID field as a hex string. Must encode 16 bytes. Unchecked if unset.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.ReportId, "report_id", []byte{}, "The expected REPORT_ID field as a hex string. Must encode 32 bytes. Unchecked if unset.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.ReportIdMa, "report_id_ma", []byte{}, "The expected REPORT_ID_MA field as a hex string. Must encode 32 bytes. Unchecked if unset.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.Measurement, "measurement", []byte{}, "The expected MEASUREMENT field as a hex string. Must encode 48 bytes. Unchecked if unset.")
 	cmd.Flags().BytesHexVar(&cfg.Policy.ChipId, "chip_id", []byte{}, "The expected MEASUREMENT field as a hex string. Must encode 48 bytes. Unchecked if unset.")
-	cmd.Flags().Uint64Var(&cfg.Policy.MinimumTcb, "minimum_tcb", 0, "The minimum acceptable value for CURRENT_TCB, COMMITTED_TCB, and REPORTED_TCB.")
-	cmd.Flags().Uint64Var(&cfg.Policy.MinimumLaunchTcb, "minimum_lauch_tcb", 0, "The minimum acceptable value for LAUNCH_TCB.")
-	cmd.Flags().Uint32Var(&cfg.Policy.MinimumGuestSvn, "minimum_guest_svn", (1 << 17), "The most acceptable SnpPolicy component-wise in its 64-bit format.")
-	cmd.Flags().Uint32Var(&cfg.Policy.MinimumBuild, "minimum_build", 0, "The 8-bit minimum build number for AMD-SP firmware")
-	cmd.Flags().BoolVar(&cfg.RootOfTrust.CheckCrl, "check_crl", false, "Download and check the CRL for revoked certificates.")
-	cmd.Flags().BoolVar(&cfg.RootOfTrust.DisallowNetwork, "disallow_network", false, "If true, then permitted to download necessary files for verification.")
-	cmd.Flags().DurationVar(&timeout, "timeout", 2*time.Minute, "Duration to continue to retry failed HTTP requests.")
-	cmd.Flags().DurationVar(&maxRetryDelay, "max_retry_delay", 30*time.Second, "Maximum Duration to wait between HTTP request retries.")
-	cmd.Flags().BoolVar(&cfg.Policy.RequireAuthorKey, "require_author_key", false, "Require that AUTHOR_KEY_EN is 1.")
-	cmd.Flags().BoolVar(&cfg.Policy.RequireIdBlock, "require_id_block", false, "Require that the VM was launch with an ID_BLOCK signed by a trusted id key or author key")
-	cmd.Flags().BoolVarP(&cfg.Policy.PermitProvisionalFirmware, "permit_privisional_software", "provisional", false, "Permit provisional firmware (i.e., committed values may be less than current values).")
+	cmd.Flags().Uint64Var(&cfg.Policy.MinimumTcb, "minimum_tcb", defaultMinimumTcb, "The minimum acceptable value for CURRENT_TCB, COMMITTED_TCB, and REPORTED_TCB.")
+	cmd.Flags().Uint64Var(&cfg.Policy.MinimumLaunchTcb, "minimum_lauch_tcb", defaultMinimumLaunchTcb, "The minimum acceptable value for LAUNCH_TCB.")
+	cmd.Flags().Uint32Var(&cfg.Policy.MinimumGuestSvn, "minimum_guest_svn", defaultMinimumGuestSvn, "The most acceptable SnpPolicy component-wise in its 64-bit format.")
+	cmd.Flags().Uint32Var(&cfg.Policy.MinimumBuild, "minimum_build", defaultMinimumBuild, "The 8-bit minimum build number for AMD-SP firmware")
+	cmd.Flags().BoolVar(&cfg.RootOfTrust.CheckCrl, "check_crl", defaultCheckCrl, "Download and check the CRL for revoked certificates.")
+	cmd.Flags().BoolVar(&cfg.RootOfTrust.DisallowNetwork, "disallow_network", defaultDisallowNetwork, "If true, then permitted to download necessary files for verification.")
+	cmd.Flags().DurationVar(&timeout, "timeout", defaultTimeout, "Duration to continue to retry failed HTTP requests.")
+	cmd.Flags().DurationVar(&maxRetryDelay, "max_retry_delay", defaultMaxRetryDelay, "Maximum Duration to wait between HTTP request retries.")
+	cmd.Flags().BoolVar(&cfg.Policy.RequireAuthorKey, "require_author_key", defaultRequireAuthor, "Require that AUTHOR_KEY_EN is 1.")
+	cmd.Flags().BoolVar(&cfg.Policy.RequireIdBlock, "require_id_block", defaultRequireIdBlock, "Require that the VM was launch with an ID_BLOCK signed by a trusted id key or author key")
+	cmd.Flags().BoolVarP(&cfg.Policy.PermitProvisionalFirmware, "permit_privisional_software", "provisional", defaultPermitProvisionalSoftware, "Permit provisional firmware (i.e., committed values may be less than current values).")
 	cmd.Flags().StringVar(&platformInfo, "platform_info", "", "The maximum acceptable PLATFORM_INFO field bit-wise. May be empty or a 64-bit unsigned integer")
 	cmd.Flags().StringVar(&cfg.Policy.MinimumVersion, "minimum_version", "", "Minimum AMD-SP firmware API version (major.minor). Each number must be 8-bit non-negative.")
 	cmd.Flags().StringArrayVar(&trustedAuthorKeys, "trusted_author_keys", []string{}, "Paths to x.509 certificates of trusted author keys")
@@ -160,10 +181,14 @@ func (cli *CLI) NewValidateAttestationValidationCmd() *cobra.Command {
 	cmd.Flags().StringVar(&stepping, "stepping", "", "The machine stepping for the chip that generated the attestation report. Default unchecked.")
 	cmd.Flags().StringArrayVar(&cfg.RootOfTrust.CabundlePaths, "CA_bundles_paths", []string{}, "Paths to CA bundles for the AMD product. Must be in PEM format, ASK, then ARK certificates. If unset, uses embedded root certificates.")
 	cmd.Flags().StringArrayVar(&cfg.RootOfTrust.Cabundles, "CA_bundles", []string{}, "PEM format CA bundles for the AMD product. Combined with contents of cabundle_paths.")
+
+	if err := cmd.MarkFlagRequired("report_data"); err != nil {
+		log.Fatalf("Failed to mark flag as required: %s", err)
+	}
 	return cmd
 }
 
-func verifyAndValidateAttestation(attestation, reportData []byte) error {
+func verifyAndValidateAttestation(attestation []byte) error {
 	sopts, err := verify.RootOfTrustToOptions(cfg.RootOfTrust)
 	if err != nil {
 		return err
@@ -185,7 +210,6 @@ func verifyAndValidateAttestation(attestation, reportData []byte) error {
 	if err != nil {
 		return err
 	}
-	opts.ReportData = reportData
 	if err = validate.SnpAttestation(attestationPB, opts); err != nil {
 		return err
 	}
@@ -211,4 +235,89 @@ func parseConfig() error {
 		cfg.Policy = &check.Policy{}
 	}
 	return nil
+}
+
+func parseHashes() error {
+	for _, hash := range trustedAuthorHashes {
+		hashBytes, err := hex.DecodeString(hash)
+		if err != nil {
+			return err
+		}
+		cfg.Policy.TrustedAuthorKeyHashes = append(cfg.Policy.TrustedAuthorKeyHashes, hashBytes)
+	}
+	for _, hash := range trustedIdKeyHashes {
+		hashBytes, err := hex.DecodeString(hash)
+		if err != nil {
+			return err
+		}
+		cfg.Policy.TrustedIdKeyHashes = append(cfg.Policy.TrustedIdKeyHashes, hashBytes)
+	}
+	return nil
+}
+
+func parseFiles() error {
+	for _, path := range trustedAuthorKeys {
+		file, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		cfg.Policy.TrustedAuthorKeys = append(cfg.Policy.TrustedAuthorKeys, file)
+	}
+	for _, path := range trustedIdKeys {
+		file, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		cfg.Policy.TrustedIdKeys = append(cfg.Policy.TrustedIdKeys, file)
+	}
+	return nil
+}
+
+func parseUints() error {
+	if stepping != "" {
+		if base := getBase(stepping); base == 10 {
+			num, err := strconv.ParseUint(stepping, getBase(stepping), 8)
+			if err != nil {
+				return err
+			}
+			cfg.Policy.Product.MachineStepping = wrapperspb.UInt32(uint32(num))
+		} else {
+			num, err := strconv.ParseUint(stepping[2:], base, 8)
+			if err != nil {
+				return err
+			}
+			cfg.Policy.Product.MachineStepping = wrapperspb.UInt32(uint32(num))
+		}
+
+	}
+	if platformInfo != "" {
+		if base := getBase(platformInfo); base == 10 {
+			num, err := strconv.ParseUint(platformInfo, getBase(platformInfo), 8)
+			if err != nil {
+				return err
+			}
+			cfg.Policy.PlatformInfo = wrapperspb.UInt64(num)
+		} else {
+			num, err := strconv.ParseUint(platformInfo[2:], base, 8)
+			if err != nil {
+				return err
+			}
+			cfg.Policy.PlatformInfo = wrapperspb.UInt64(num)
+		}
+	}
+	return nil
+}
+
+func getBase(val string) int {
+
+	switch {
+	case strings.HasPrefix(val, "0x"):
+		return 16
+	case strings.HasPrefix(val, "0o"):
+		return 8
+	case strings.HasPrefix(val, "0b"):
+		return 2
+	default:
+		return 10
+	}
 }
