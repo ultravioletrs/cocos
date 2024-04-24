517a482ca2dc970dca73fa8ffb8dab65f8875b40
danko-miladinovic <danko@etf.rs>
2024-04-24 13:06:08 +0200

    fixed small typo

--- init/systemd/cocos_network_setup.sh
+++ init/systemd/cocos_network_setup.sh
@@ -3,7 +3,7 @@
 # IFACES are all network interfaces excluding lo (LOOPBACK) and sit interfaces 
 IFACES=$(ip link show | grep -vE 'LOOPBACK|sit*' | awk -F': ' '{print $2}')
 
-# This for loop brings up all network interfaces in IFACE and dhclient obtains an IP address for the every interface
+# This for loop brings up all network interfaces in IFACES and dhclient obtains an IP address for the every interface
 for IFACE in $IFACES; do
     STATE=$(ip link show $IFACE | grep DOWN)
     if [ -n "$STATE" ]; then
