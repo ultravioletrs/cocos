2efa327a8f62eeeaedc03cb1971db945c20890b5
Sammy Oina <sammyoina@gmail.com>
2024-07-18 15:31:12 +0300

    simplify
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- agent/algorithm/binary/binary.go
+++ agent/algorithm/binary/binary.go
@@ -14,8 +14,6 @@ import (
 	"github.com/ultravioletrs/cocos/agent/events"
 )
 
-const socketPath = "unix_socket"
-
 var _ algorithm.Algorithm = (*binary)(nil)
 
 type binary struct {
@@ -37,7 +35,7 @@ func New(logger *slog.Logger, eventsSvc events.Service, algoFile string, dataset
 }
 
 func (b *binary) Run() ([]byte, error) {
-	b.logger.Debug("testing stderr")
+	b.logger.Debug("testing stderr 2")
 	defer os.Remove(b.algoFile)
 	defer func() {
 		for _, file := range b.datasets {
@@ -45,15 +43,10 @@ func (b *binary) Run() ([]byte, error) {
 		}
 	}()
 
-	// Create channels for received data and errors
-	dataChannel := make(chan []byte)
-	errorChannel := make(chan error)
-
-	var result []byte
-
+	var outBuf bytes.Buffer
 	cmd := exec.Command(b.algoFile, b.datasets...)
 	cmd.Stderr = b.stderr
-	cmd.Stdout = &Stdout{DataChan: dataChannel, ErrorChan: errorChannel}
+	cmd.Stdout = io.MultiWriter(&outBuf, b.stdout)
 
 	if err := cmd.Start(); err != nil {
 		return nil, fmt.Errorf("error starting algorithm: %v", err)
@@ -63,41 +56,5 @@ func (b *binary) Run() ([]byte, error) {
 		return nil, fmt.Errorf("algorithm execution error: %v", err)
 	}
 
-	select {
-	case result = <-dataChannel:
-		return result, nil
-	case err := <-errorChannel:
-		return nil, fmt.Errorf("error receiving data: %v", err)
-	}
-}
-
-var _ io.Writer = &Stdout{}
-
-const bufSize = 1024
-
-type Stdout struct {
-	DataChan  chan []byte
-	ErrorChan chan error
-}
-
-// Write implements io.Writer.
-func (s *Stdout) Write(p []byte) (n int, err error) {
-	inBuf := bytes.NewBuffer(p)
-
-	buf := make([]byte, bufSize)
-
-	for {
-		n, err := inBuf.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			s.ErrorChan <- err
-			return len(p) - inBuf.Len(), err
-		}
-
-		s.DataChan <- buf[:n]
-	}
-
-	return len(p), nil
+	return outBuf.Bytes(), nil
 }
