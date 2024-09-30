23bb431fd669be9a29cced18f1d2ed3f49e05a69
Sammy Oina <sammyoina@gmail.com>
2024-09-30 13:21:11 +0300

    add pkg tests
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- pkg/clients/grpc/connect.go
+++ pkg/clients/grpc/connect.go
@@ -68,7 +68,7 @@ type Config struct {
 	URL          string        `env:"URL"             envDefault:"localhost:7001"`
 	Timeout      time.Duration `env:"TIMEOUT"         envDefault:"60s"`
 	AttestedTLS  bool          `env:"ATTESTED_TLS"    envDefault:"false"`
-	Manifest     string        `env:"MANIFEST"        envDefault:""`
+	BackendInfo  string        `env:"BACKEND_INFO"    envDefault:""`
 }
 
 type AttestationConfiguration struct {
@@ -142,7 +142,7 @@ func connect(cfg Config) (*grpc.ClientConn, security, error) {
 	tc := insecure.NewCredentials()
 
 	if cfg.AttestedTLS {
-		err := ReadManifest(cfg.Manifest, &attestationConfiguration)
+		err := ReadManifest(cfg.BackendInfo, &attestationConfiguration)
 		if err != nil {
 			return nil, secure, fmt.Errorf("failed to read Manifest %w", err)
 		}
