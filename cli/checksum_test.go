253287c5511f32db6fde25b3be16ffbf2d5ef823
Sammy Oina <sammyoina@gmail.com>
2024-11-08 15:21:01 +0300

    add test cases
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- cli/checksum_test.go
+++ cli/checksum_test.go
@@ -4,11 +4,12 @@ package cli
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"strings"
 	"testing"
 
-	"github.com/ultravioletrs/cocos/internal"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewFileHashCmd(t *testing.T) {
@@ -29,74 +30,199 @@ func TestNewFileHashCmd(t *testing.T) {
 }
 
 func TestNewFileHashCmdRun(t *testing.T) {
-	cli := &CLI{}
-	cmd := cli.NewFileHashCmd()
-
-	content := []byte("test content")
-	tmpfile, err := os.CreateTemp("", "example")
-	if err != nil {
-		t.Fatal(err)
+	testCases := []struct {
+		name        string
+		isManifest  bool
+		toBase64    bool
+		expectedOut string
+		expectedErr string
+	}{
+		{
+			name:        "Valid file",
+			isManifest:  false,
+			toBase64:    false,
+			expectedOut: "Hash of file:",
+			expectedErr: "",
+		},
+		{
+			name:        "Valid manifest file",
+			isManifest:  true,
+			toBase64:    false,
+			expectedOut: "Hash of manifest file:",
+			expectedErr: "",
+		},
+		{
+			name:        "Valid file with base64 output",
+			isManifest:  false,
+			toBase64:    true,
+			expectedOut: "Hash of file:",
+			expectedErr: "",
+		},
+		{
+			name:        "Non-existent file",
+			isManifest:  false,
+			toBase64:    false,
+			expectedOut: "Error computing hash:",
+			expectedErr: "",
+		},
 	}
-	defer os.Remove(tmpfile.Name())
 
-	if _, err := tmpfile.Write(content); err != nil {
-		t.Fatal(err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cli := &CLI{}
+			cmd := cli.NewFileHashCmd()
 
-	var output bytes.Buffer
-	cmd.SetOut(&output)
-	cmd.SetErr(&output)
+			var output bytes.Buffer
+			cmd.SetOut(&output)
+			cmd.SetErr(&output)
 
-	cmd.SetArgs([]string{tmpfile.Name()})
-	err = cmd.Execute()
-	if err != nil {
-		t.Fatalf("Error executing command: %v", err)
-	}
+			cmd.Flags().Set("manifest", fmt.Sprint(tc.isManifest))
+			cmd.Flags().Set("base64", fmt.Sprint(tc.toBase64))
 
-	expectedHash, err := internal.ChecksumHex(tmpfile.Name())
-	if err != nil {
-		t.Fatalf("Error computing expected hash: %v", err)
-	}
+			if tc.name == "Non-existent file" {
+				cmd.SetArgs([]string{"non_existent_file.txt"})
+			} else {
+				content := []byte("{}")
+				tmpfile, err := os.CreateTemp("", "example")
+				if err != nil {
+					t.Fatal(err)
+				}
+				defer os.Remove(tmpfile.Name())
 
-	if !strings.Contains(output.String(), expectedHash) {
-		t.Errorf("Expected output to contain hash %s, got %s", expectedHash, output.String())
+				if _, err := tmpfile.Write(content); err != nil {
+					t.Fatal(err)
+				}
+				if err := tmpfile.Close(); err != nil {
+					t.Fatal(err)
+				}
+
+				cmd.SetArgs([]string{tmpfile.Name()})
+			}
+
+			err := cmd.Execute()
+			if err != nil {
+				t.Fatalf("Error executing command: %v", err)
+			}
+
+			out := output.String()
+			if !strings.Contains(out, tc.expectedOut) {
+				t.Errorf("Expected output to contain '%s', got '%s'", tc.expectedOut, out)
+			}
+
+			if tc.expectedErr != "" && !strings.Contains(out, tc.expectedErr) {
+				t.Errorf("Expected output to contain '%s', got '%s'", tc.expectedErr, out)
+			}
+		})
 	}
 }
 
-func TestNewFileHashCmdInvalidArgs(t *testing.T) {
-	cli := &CLI{}
-	cmd := cli.NewFileHashCmd()
-
-	err := cmd.Execute()
-	if err == nil {
-		t.Error("Expected error when executing without arguments, got nil")
+func TestManifestChecksum(t *testing.T) {
+	testCases := []struct {
+		name        string
+		jsonContent string
+		expectedSum string
+	}{
+		{
+			name: "Valid manifest file",
+			jsonContent: `{
+				"id": "1234",
+				"name": "Example Computation",
+				"description": "This is an example computation"
+			}`,
+			expectedSum: "868825367c32c4b6d621d5d95e2890f233d8554df2348ab743aac2663a936f08",
+		},
+		{
+			name:        "Invalid JSON",
+			jsonContent: `{`,
+			expectedSum: "",
+		},
 	}
 
-	cmd.SetArgs([]string{"file1", "file2"})
-	err = cmd.Execute()
-	if err == nil {
-		t.Error("Expected error when executing with too many arguments, got nil")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := os.CreateTemp("", "test")
+			assert.Nil(t, err)
+
+			t.Cleanup(func() {
+				os.Remove(f.Name())
+			})
+
+			_, err = f.Write([]byte(tc.jsonContent))
+			assert.Nil(t, err)
+
+			err = f.Close()
+			assert.Nil(t, err)
+
+			hash, err := manifestChecksum(f.Name())
+			if tc.expectedSum == "" && err == nil {
+				t.Errorf("Expected error, got nil")
+			}
+			if tc.expectedSum != "" && err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			if hash != tc.expectedSum {
+				t.Errorf("Expected hash %s, got %s", tc.expectedSum, hash)
+			}
+		})
 	}
 }
 
-func TestNewFileHashCmdNonExistentFile(t *testing.T) {
-	cli := &CLI{}
-	cmd := cli.NewFileHashCmd()
-
-	var output bytes.Buffer
-	cmd.SetOut(&output)
-	cmd.SetErr(&output)
-
-	cmd.SetArgs([]string{"non_existent_file.txt"})
-	err := cmd.Execute()
-	if err != nil {
-		t.Fatalf("Error executing command: %v", err)
+func TestHexToBase64(t *testing.T) {
+	testCases := []struct {
+		name        string
+		hexInput    string
+		expectedOut string
+	}{
+		{
+			name:        "Valid hex input",
+			hexInput:    "48656c6c6f",
+			expectedOut: "SGVsbG8=",
+		},
+		{
+			name:        "Invalid hex input",
+			hexInput:    "invalid-hex",
+			expectedOut: "",
+		},
 	}
 
-	if !strings.Contains(output.String(), "Error computing hash") {
-		t.Errorf("Expected output to contain 'Error computing hash', got %s", output.String())
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			out := hexToBase64(tc.hexInput)
+			if out != tc.expectedOut {
+				t.Errorf("Expected %s, got %s", tc.expectedOut, out)
+			}
+		})
+	}
+}
+
+func TestHashOut(t *testing.T) {
+	testCases := []struct {
+		name        string
+		hashHex     string
+		toBase64    bool
+		expectedOut string
+	}{
+		{
+			name:        "Hex output",
+			hashHex:     "48656c6c6f",
+			toBase64:    false,
+			expectedOut: "48656c6c6f",
+		},
+		{
+			name:        "Base64 output",
+			hashHex:     "48656c6c6f",
+			toBase64:    true,
+			expectedOut: "SGVsbG8=",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			toBase64 = tc.toBase64
+			out := hashOut(tc.hashHex)
+			if out != tc.expectedOut {
+				t.Errorf("Expected %s, got %s", tc.expectedOut, out)
+			}
+		})
 	}
 }
