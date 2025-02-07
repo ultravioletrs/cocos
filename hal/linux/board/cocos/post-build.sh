ae7912c6d91f6ef68532d59e7350340c9df22634
Sammy Oina <sammyoina@gmail.com>
2025-02-07 15:51:24 +0300

    Remove redundant mount point creation and fstab entries in post-build script
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- hal/linux/board/cocos/post-build.sh
+++ hal/linux/board/cocos/post-build.sh
@@ -9,20 +9,3 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
     sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
-
-# Create the mount points
-# Create the mount points
-mkdir -p ${TARGET_DIR}/etc/certs
-mkdir -p ${TARGET_DIR}/etc/cocos
-
-# Ensure /etc/fstab exists
-if [ ! -f "${TARGET_DIR}/etc/fstab" ]; then
-    touch "${TARGET_DIR}/etc/fstab"
-fi
-
-# Add the 9p entries to /etc/fstab
-grep -q "certs_share /etc/certs" ${TARGET_DIR}/etc/fstab || \
-echo "certs_share /etc/certs 9p trans=virtio,version=9p2000.L,cache=mmap 0 0" >> "${TARGET_DIR}/etc/fstab"
-
-grep -q "env_share /etc/cocos" ${TARGET_DIR}/etc/fstab || \
-echo "env_share /etc/cocos 9p trans=virtio,version=9p2000.L,cache=mmap 0 0" >> "${TARGET_DIR}/etc/fstab"
