495daca667fe1d2b261d34fb37d06f06863224d2
Sammy Oina <sammyoina@gmail.com>
2026-01-12 16:31:57 +0300

    feat: Introduce TLS package for enhanced security configuration and refactor client code to utilize new TLS utilities
    Signed-off-by: Sammy Oina <sammyoina@gmail.com>

--- pkg/clients/http/client.go
+++ pkg/clients/http/client.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/ultravioletrs/cocos/pkg/clients"
+	"github.com/ultravioletrs/cocos/pkg/tls"
 )
 
 type Client interface {
@@ -19,7 +20,7 @@ type Client interface {
 type client struct {
 	transport *http.Transport
 	cfg       clients.ClientConfiguration
-	security  clients.Security
+	security  tls.Security
 }
 
 var _ Client = (*client)(nil)
@@ -49,17 +50,22 @@ func (c *client) Timeout() time.Duration {
 	return c.cfg.Config().Timeout
 }
 
-func createTransport(cfg clients.ClientConfiguration) (*http.Transport, clients.Security, error) {
+func createTransport(cfg clients.ClientConfiguration) (*http.Transport, tls.Security, error) {
 	transport := &http.Transport{
 		MaxIdleConns:        100,
 		IdleConnTimeout:     90 * time.Second,
 		TLSHandshakeTimeout: 10 * time.Second,
 	}
 
-	security := clients.WithoutTLS
+	security := tls.WithoutTLS
 
 	if agcfg, ok := cfg.(*clients.AttestedClientConfig); ok && agcfg.AttestedTLS {
-		result, err := clients.LoadATLSConfig(*agcfg)
+		result, err := tls.LoadATLSConfig(
+			agcfg.AttestationPolicy,
+			agcfg.ServerCAFile,
+			agcfg.ClientCert,
+			agcfg.ClientKey,
+		)
 		if err != nil {
 			return nil, security, err
 		}
@@ -69,12 +75,12 @@ func createTransport(cfg clients.ClientConfiguration) (*http.Transport, clients.
 	} else {
 		conf := cfg.Config()
 
-		result, err := clients.LoadBasicTLSConfig(conf.ServerCAFile, conf.ClientCert, conf.ClientKey)
+		result, err := tls.LoadBasicConfig(conf.ServerCAFile, conf.ClientCert, conf.ClientKey)
 		if err != nil {
 			return nil, security, err
 		}
 
-		if result.Security != clients.WithoutTLS {
+		if result.Security != tls.WithoutTLS {
 			transport.TLSClientConfig = result.Config
 		}
 
