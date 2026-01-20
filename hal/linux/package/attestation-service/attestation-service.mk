e1d6f372343ff1492d185aab5f5f0e1e17cea61a
Sammy Oina <sammyoina@gmail.com>
2026-01-20 23:07:37 +0300

    fix: Move ifeq conditional outside define block in attestation-service.mk
    Make conditionals cannot be evaluated inside define...endef blocks
    when used as recipe bodies. Restructured to define the
    ATTESTATION_SERVICE_INSTALL_INIT_SYSTEMD block conditionally based
    on BR2_PACKAGE_CC_ATTESTATION_AGENT configuration.

--- hal/linux/package/attestation-service/attestation-service.mk
+++ hal/linux/package/attestation-service/attestation-service.mk
@@ -15,17 +15,21 @@ define ATTESTATION_SERVICE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/build/cocos-attestation-service $(TARGET_DIR)/usr/bin/attestation-service
 endef
 
+ifeq ($(BR2_PACKAGE_CC_ATTESTATION_AGENT),y)
 define ATTESTATION_SERVICE_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0640 $(@D)/init/systemd/attestation-service.service $(TARGET_DIR)/usr/lib/systemd/system/attestation-service.service
 	$(INSTALL) -D -m 0750 $(@D)/init/systemd/attestation_setup.sh $(TARGET_DIR)/cocos_init/attestation_setup.sh
-ifeq ($(BR2_PACKAGE_CC_ATTESTATION_AGENT),y)
 	# Enable CC attestation agent backend
 	sed -i 's/USE_CC_ATTESTATION_AGENT=false/USE_CC_ATTESTATION_AGENT=true/' $(TARGET_DIR)/usr/lib/systemd/system/attestation-service.service
+endef
 else
+define ATTESTATION_SERVICE_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0640 $(@D)/init/systemd/attestation-service.service $(TARGET_DIR)/usr/lib/systemd/system/attestation-service.service
+	$(INSTALL) -D -m 0750 $(@D)/init/systemd/attestation_setup.sh $(TARGET_DIR)/cocos_init/attestation_setup.sh
 	# Disable CC attestation agent backend
 	sed -i 's/USE_CC_ATTESTATION_AGENT=true/USE_CC_ATTESTATION_AGENT=false/' $(TARGET_DIR)/usr/lib/systemd/system/attestation-service.service
 	sed -i '/Wants=attestation-agent.service/d' $(TARGET_DIR)/usr/lib/systemd/system/attestation-service.service
-endif
 endef
+endif
 
 $(eval $(generic-package))
